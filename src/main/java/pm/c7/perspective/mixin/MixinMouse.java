a78c11fab653b28bd77b56fd7e72623132f818ae
Cynthia Foxwell <lumodder@gmail.com>
2019-08-06 11:31:56 -0600

    fix smooth/cinematic camera

--- src/main/java/pm/c7/perspective/mixin/MixinMouse.java
+++ src/main/java/pm/c7/perspective/mixin/MixinMouse.java
@@ -3,6 +3,7 @@ package pm.c7.perspective.mixin;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.Mouse;
 import net.minecraft.client.util.GlfwUtil;
+import net.minecraft.client.util.SmoothUtil;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -22,6 +23,10 @@ public class MixinMouse {
     private double field_1785 = Double.MIN_VALUE;
     @Shadow
     private boolean isCursorLocked;
+    @Shadow
+    private final SmoothUtil cursorXSmoother = new SmoothUtil();
+    @Shadow
+    private final SmoothUtil cursorYSmoother = new SmoothUtil();
 
     @Inject(method="updateMouse",at=@At("HEAD"),cancellable=true)
     private void noPlayerRotation(CallbackInfo info) {
@@ -29,34 +34,48 @@ public class MixinMouse {
         double min = time - this.field_1785;
         this.field_1785 = time;
         if (this.isCursorLocked() && this.client.isWindowFocused()) {
-            if (PerspectiveMod.INSTANCE.PERSPECTIVE_ENABLED) {
-                double sens = this.client.options.mouseSensitivity * 0.6000000238418579D + 0.20000000298023224D;
-                double mult = sens * sens * sens * 8.0D;
-                double deltaX = this.cursorDeltaX * mult;
-                double deltaY = this.cursorDeltaY * mult;
+            double sens = this.client.options.mouseSensitivity * 0.6000000238418579D + 0.20000000298023224D;
+            double mult = sens * sens * sens * 8.0D;
+            double deltaX;
+            double deltaY;
+            if (this.client.options.smoothCameraEnabled) {
+                double smoothX = this.cursorXSmoother.smooth(this.cursorDeltaX * mult, min * mult);
+                double smoothY = this.cursorYSmoother.smooth(this.cursorDeltaY * mult, min * mult);
+                deltaX = smoothX;
+                deltaY = smoothY;
+            } else {
+                this.cursorXSmoother.clear();
+                this.cursorYSmoother.clear();
+                deltaX = this.cursorDeltaX * mult;
+                deltaY = this.cursorDeltaY * mult;
+            }
 
+            if (PerspectiveMod.INSTANCE.PERSPECTIVE_ENABLED) {
                 PerspectiveMod.INSTANCE.cameraYaw += deltaX / 8.0D;
                 PerspectiveMod.INSTANCE.cameraPitch += deltaY / 8.0D;
 
                 if (Math.abs(PerspectiveMod.INSTANCE.cameraPitch) > 90.0f) {
                     PerspectiveMod.INSTANCE.cameraPitch = (PerspectiveMod.INSTANCE.cameraPitch > 0.0F) ? 90.0F : -90.0F;
                 }
-
-                this.cursorDeltaX = 0.0D;
-                this.cursorDeltaY = 0.0D;
-                int yDir = 1;
-                if (this.client.options.invertYMouse) {
-                    yDir = -1;
-                }
-
-                /*this.client.getTutorialManager().onUpdateMouse(deltaX, deltaY);
-                if (this.client.player != null) {
-                    this.client.player.changeLookDirection(deltaX, deltaY * (double)yDir);
-                }*/
-
-                info.cancel();
             }
+
+            this.cursorDeltaX = 0.0D;
+            this.cursorDeltaY = 0.0D;
+            int yDir = 1;
+            if (this.client.options.invertYMouse) {
+                yDir = -1;
+            }
+
+            this.client.getTutorialManager().onUpdateMouse(deltaX, deltaY);
+            if (this.client.player != null && !PerspectiveMod.INSTANCE.PERSPECTIVE_ENABLED) {
+                this.client.player.changeLookDirection(deltaX, deltaY * (double)yDir);
+            }
+        }else{
+            this.cursorDeltaX = 0.0D;
+            this.cursorDeltaY = 0.0D;
         }
+
+        info.cancel();
     }
 
     @Shadow
