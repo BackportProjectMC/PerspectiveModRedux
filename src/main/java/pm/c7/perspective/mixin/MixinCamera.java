6b736ae8cdb8af9d0d0c32a505bdc6dad4b07542
Cynthia Foxwell <lumodder@gmail.com>
2020-11-10 20:10:44 -0700

    Ironically port 1.8.9 backport mixins (to an extent) (better mod compat)

--- src/main/java/pm/c7/perspective/mixin/MixinCamera.java
+++ src/main/java/pm/c7/perspective/mixin/MixinCamera.java
@@ -8,21 +8,44 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 import pm.c7.perspective.PerspectiveMod;
 
 @Mixin(Camera.class)
 public class MixinCamera {
     @Shadow
+    private float pitch;
+    @Shadow
+    private float yaw;
+
+    @Inject(method = "update", at = @At(value = "INVOKE", target = "net/minecraft/client/render/Camera.moveBy(DDD)V", ordinal = 0))
+    private void perspectiveUpdatePitchYaw(BlockView area, Entity focusedEntity, boolean thirdPerson, boolean inverseView, float tickDelta, CallbackInfo info) {
+        if (PerspectiveMod.INSTANCE.perspectiveEnabled) {
+            this.pitch = PerspectiveMod.INSTANCE.cameraPitch;
+            this.yaw = PerspectiveMod.INSTANCE.cameraYaw;
+        }
+    }
+
+    @ModifyArgs(method = "update", at = @At(value = "INVOKE", target = "net/minecraft/client/render/Camera.setRotation(FF)V", ordinal = 0))
+    private void perspectiveFixRotation(Args args) {
+        if (PerspectiveMod.INSTANCE.perspectiveEnabled) {
+            args.set(0, PerspectiveMod.INSTANCE.cameraYaw);
+            args.set(1, PerspectiveMod.INSTANCE.cameraPitch);
+        } /*else {
+            args.set(0, args.get(0));
+            args.set(1, args.get(1));
+        }*/
+    }
+
+    /*@Shadow
     private boolean ready;
     @Shadow
     private BlockView area;
     @Shadow
     private Entity focusedEntity;
-    @Shadow
-    private float pitch;
-    @Shadow
-    private float yaw;
+
     @Shadow
     private boolean thirdPerson;
     @Shadow
@@ -60,5 +83,5 @@ public class MixinCamera {
     @Shadow
     private double clipToSpace(double desiredCameraDistance) {
         return desiredCameraDistance;
-    }
+    }*/
 }
--- src/main/java/pm/c7/perspective/mixin/MixinMouse.java
+++ src/main/java/pm/c7/perspective/mixin/MixinMouse.java
@@ -2,84 +2,49 @@ package pm.c7.perspective.mixin;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.Mouse;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.util.GlfwUtil;
 import net.minecraft.client.util.SmoothUtil;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import pm.c7.perspective.PerspectiveMod;
 
 @Mixin(Mouse.class)
 public class MixinMouse {
-    @Shadow
-    private MinecraftClient client;
-    @Shadow
-    private double cursorDeltaX;
-    @Shadow
-    private double cursorDeltaY;
-    @Shadow
-    private double lastMouseUpdateTime = 4.9E-324D;
-    @Shadow
-    private boolean cursorLocked;
-    @Shadow
-    private final SmoothUtil cursorXSmoother = new SmoothUtil();
-    @Shadow
-    private final SmoothUtil cursorYSmoother = new SmoothUtil();
+    @Inject(
+        method = "updateMouse",
+        at = @At(
+            value = "INVOKE",
+            target = "net/minecraft/client/tutorial/TutorialManager.onUpdateMouse(DD)V"
+        ),
+        locals = LocalCapture.CAPTURE_FAILEXCEPTION
+    )
+    private void perspectiveUpdatePitchYaw(CallbackInfo info, double adjustedSens, double x, double y, int invert) {
+        if (PerspectiveMod.INSTANCE.perspectiveEnabled) {
+            PerspectiveMod.INSTANCE.cameraYaw += x / 8.0F;
+            PerspectiveMod.INSTANCE.cameraPitch += (y * invert) / 8.0F;
 
-    @Inject(method="updateMouse",at=@At("HEAD"),cancellable=true)
-    private void noPlayerRotation(CallbackInfo info) {
-        double time = GlfwUtil.getTime();
-        double min = time - this.lastMouseUpdateTime;
-        this.lastMouseUpdateTime = time;
-        if (this.isCursorLocked() && this.client.isWindowFocused()) {
-            double sens = this.client.options.mouseSensitivity * 0.6000000238418579D + 0.20000000298023224D;
-            double mult = sens * sens * sens * 8.0D;
-            double deltaX;
-            double deltaY;
-            if (this.client.options.smoothCameraEnabled) {
-                double smoothX = this.cursorXSmoother.smooth(this.cursorDeltaX * mult, min * mult);
-                double smoothY = this.cursorYSmoother.smooth(this.cursorDeltaY * mult, min * mult);
-                deltaX = smoothX;
-                deltaY = smoothY;
-            } else {
-                this.cursorXSmoother.clear();
-                this.cursorYSmoother.clear();
-                deltaX = this.cursorDeltaX * mult;
-                deltaY = this.cursorDeltaY * mult;
+            if (Math.abs(PerspectiveMod.INSTANCE.cameraPitch) > 90.0F) {
+                PerspectiveMod.INSTANCE.cameraPitch = PerspectiveMod.INSTANCE.cameraPitch > 0.0F ? 90.0F : -90.0F;
             }
-
-            if (PerspectiveMod.INSTANCE.perspectiveEnabled) {
-                PerspectiveMod.INSTANCE.cameraYaw += deltaX / 8.0D;
-                PerspectiveMod.INSTANCE.cameraPitch += deltaY / 8.0D;
-
-                if (Math.abs(PerspectiveMod.INSTANCE.cameraPitch) > 90.0f) {
-                    PerspectiveMod.INSTANCE.cameraPitch = (PerspectiveMod.INSTANCE.cameraPitch > 0.0F) ? 90.0F : -90.0F;
-                }
-            }
-
-            this.cursorDeltaX = 0.0D;
-            this.cursorDeltaY = 0.0D;
-            int yDir = 1;
-            if (this.client.options.invertYMouse) {
-                yDir = -1;
-            }
-
-            this.client.getTutorialManager().onUpdateMouse(deltaX, deltaY);
-            if (this.client.player != null && !PerspectiveMod.INSTANCE.perspectiveEnabled) {
-                this.client.player.changeLookDirection(deltaX, deltaY * (double)yDir);
-            }
-        }else{
-            this.cursorDeltaX = 0.0D;
-            this.cursorDeltaY = 0.0D;
         }
-
-        info.cancel();
     }
 
-    @Shadow
-    public boolean isCursorLocked() {
-        return this.cursorLocked;
+    @Redirect(
+        method = "updateMouse",
+        at = @At(
+            value = "INVOKE",
+            target = "net/minecraft/client/network/ClientPlayerEntity.changeLookDirection(DD)V"
+        )
+    )
+    private void perspectivePreventPlayerMovement(ClientPlayerEntity player, double x, double y) {
+        if (!PerspectiveMod.INSTANCE.perspectiveEnabled) {
+            player.changeLookDirection(x, y);
+        }
     }
 }
