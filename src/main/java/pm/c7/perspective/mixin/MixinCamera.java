6ecbc4c9dee61cfd6d5af439d9582ca5cc95c79c
Cynthia Foxwell <lumodder@gmail.com>
2020-11-10 20:11:23 -0700

    Cleanup MixinCamera

--- src/main/java/pm/c7/perspective/mixin/MixinCamera.java
+++ src/main/java/pm/c7/perspective/mixin/MixinCamera.java
@@ -33,55 +33,6 @@ public class MixinCamera {
         if (PerspectiveMod.INSTANCE.perspectiveEnabled) {
             args.set(0, PerspectiveMod.INSTANCE.cameraYaw);
             args.set(1, PerspectiveMod.INSTANCE.cameraPitch);
-        } /*else {
-            args.set(0, args.get(0));
-            args.set(1, args.get(1));
-        }*/
-    }
-
-    /*@Shadow
-    private boolean ready;
-    @Shadow
-    private BlockView area;
-    @Shadow
-    private Entity focusedEntity;
-
-    @Shadow
-    private boolean thirdPerson;
-    @Shadow
-    private boolean inverseView;
-    @Shadow
-    private float cameraY;
-    @Shadow
-    private float lastCameraY;
-
-    @Inject(method="update",at=@At("HEAD"),cancellable=true)
-    private void updateCamera(BlockView area, Entity entity, boolean thirdPerson, boolean inverseView, float tickDelta, CallbackInfo info){
-        if (PerspectiveMod.INSTANCE.perspectiveEnabled) {
-            this.ready = true;
-            this.area = area;
-            this.focusedEntity = entity;
-            this.thirdPerson = thirdPerson;
-            this.inverseView = inverseView;
-
-            this.setRotation(PerspectiveMod.INSTANCE.cameraYaw, PerspectiveMod.INSTANCE.cameraPitch);
-            this.setPos(MathHelper.lerp((double)tickDelta, focusedEntity.prevX, focusedEntity.getX()), MathHelper.lerp((double)tickDelta, focusedEntity.prevY, focusedEntity.getY()) + (double)MathHelper.lerp(tickDelta, this.lastCameraY, this.cameraY), MathHelper.lerp((double)tickDelta, focusedEntity.prevZ, focusedEntity.getZ()));
-            this.pitch = PerspectiveMod.INSTANCE.cameraPitch;
-            this.yaw = PerspectiveMod.INSTANCE.cameraYaw;
-            this.moveBy(-this.clipToSpace(4.0D), 0.0D, 0.0D);
-
-            info.cancel();
         }
     }
-
-    @Shadow
-    protected void setRotation(float yaw, float pitch) {}
-    @Shadow
-    protected void setPos(double x, double y, double z) {}
-    @Shadow
-    protected void moveBy(double x, double y, double z) {}
-    @Shadow
-    private double clipToSpace(double desiredCameraDistance) {
-        return desiredCameraDistance;
-    }*/
 }
